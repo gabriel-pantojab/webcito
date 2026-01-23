2a4de6be3cee4c63288431d9fa5891576c9bc00d
Gabriel Pantoja Bustamante <pantojab.gabriel@gmail.com>
2026-01-22 21:19:43 -0400

    feat: 📌add dummy home module

--- src/angular/app/app.routes.ts
+++ src/angular/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () =>
+      import('./modules/home/home-module').then((m) => m.HomeModule),
+  },
+];
