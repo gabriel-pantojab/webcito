885d1670ba0e11e443b83cc1488cf4a95b67347f
Gabriel Pantoja Bustamante <pantojab.gabriel@gmail.com>
2026-02-17 10:57:54 -0400

    fix: 🐛 imports

--- src/angular/app/modules/enrollment-process/pages/enrollment-info-page/enrollment-info-page.ts
+++ src/angular/app/modules/enrollment-process/pages/enrollment-info-page/enrollment-info-page.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
-import { CoursesDetailsContainer } from '@modules/enrollment-process/containers/courses-details-container/courses-details-container';
 import { BasicInformationContainer } from '../../containers/basic-information-container/basic-information-container';
+import { CoursesDetailsContainer } from '../../containers/courses-details-container/courses-details-container';
 
 @Component({
   selector: 'enrollment-info-page',
