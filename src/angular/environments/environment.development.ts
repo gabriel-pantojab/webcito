27f165d0a5c5cf21a179aaabb597d4fb4c7ef0c1
Gabriel Pantoja Bustamante <pantojab.gabriel@gmail.com>
2026-02-17 13:15:30 -0400

    fix: 🐛 correct format of the id elements

--- src/angular/environments/environment.development.ts
+++ src/angular/environments/environment.development.ts
@@ -3,15 +3,15 @@ import { WEBSISConfig } from '@core/interfaces/config/websis.config';
 export const WEBSIS: WEBSISConfig = {
   url: 'https://websis.umss.edu.bo/serv_estudiantes.asp',
   login: {
-    sis: 'idCuenta',
-    password: 'idContrasena',
-    day: 'idDia',
-    month: 'idMes',
-    year: 'idMes',
-    captcha: 'idCodigo',
+    sis: '#idCuenta',
+    password: '#idContrasena',
+    day: '#idDia',
+    month: '#idMes',
+    year: '#idMes',
+    captcha: '#idCodigo',
   },
   enrollment: {
-    codeOne: 'idInput1',
-    codeTwo: 'idInput2',
+    codeOne: '#idInput1',
+    codeTwo: '#idInput2',
   },
 };
